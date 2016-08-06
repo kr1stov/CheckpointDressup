1705428973b9fc0128e8d9c33ff797f701717597
Michel Wacker <mw@gentletroll.com>
2016-08-06 17:12:34 +0200

    created enums
    Signed-off-by: Michel Wacker <mw@gentletroll.com>

---
+++ src/enums/BodyPart.js
@@ -0,0 +1,18 @@
+/**
+ * @author       Michel Wacker <mw@gentletroll.com>
+ * @copyright    2015 Gentle Troll Entertainment GmbH
+ */
+/**
+ * @namespace Fashion.BodyPart
+ */
+Fashion.BodyPart = {
+    EYES : 'eyes',
+    FACE : 'face',
+    HAIR : 'hair',
+    TORSO : 'torso',
+    ARMS : 'arms',
+    HANDS : 'hands',
+    THIGHS : 'thighs',
+    CALVES : 'calves',
+    FEET : 'feet'
+};
---
+++ src/enums/ClothingStyle.js
@@ -0,0 +1,14 @@
+/**
+ * @author       Michel Wacker <mw@gentletroll.com>
+ * @copyright    2015 Gentle Troll Entertainment GmbH
+ */
+/**
+ * @namespace Fashion.ClothingStyle
+ */
+Fashion.ClothingStyle = {
+    EXPOSED : 'exposed',
+    RELAXED : 'relaxed',
+    MODERATE : 'moderate',
+    MILITARY : 'military',
+    STRICT : 'strict'
+};
