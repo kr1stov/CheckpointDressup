fa5ef3d9f32a189ad463de160b5368ad06686c9e
Michel Wacker <mw@gentletroll.com>
2016-08-06 19:11:32 +0200

    fix phone details
    Signed-off-by: Michel Wacker <mw@gentletroll.com>

--- src/phone/Phone.js
+++ src/phone/Phone.js
@@ -7,7 +7,7 @@
  */
 
 /**
- * @class Fashion.phone
+ * @class Fashion.Phone
  *
  * @classdesc Create a new 'phone' object.
  * @constructor
@@ -20,7 +20,7 @@
  * @param {boolean} [enableBody=false] - If true all Sprites created with {@link #create} or {@link #createMulitple} will have a physics body created on them. Change the body type with {@link #physicsBodyType}.
  * @param {integer} [physicsBodyType=0] - The physics body type to use when physics bodies are automatically added. See {@link #physicsBodyType} for values.
  */
-Fashion.phone = function (game, parent, name, addToStage, enableBody, physicsBodyType) {
+Fashion.Phone = function (game, parent, name, addToStage, enableBody, physicsBodyType) {
     // call super constructor
     Phaser.Group.call(this, game, parent, name, addToStage, enableBody, physicsBodyType);
 
@@ -28,8 +28,8 @@ Fashion.phone = function (game, parent, name, addToStage, enableBody, physicsBod
 };
 
 // extend class Phaser.Group
-Fashion.phone.prototype = Object.create(Phaser.Group.prototype);
-Fashion.phone.prototype.constructor = Fashion.phone;
+Fashion.Phone.prototype = Object.create(Phaser.Group.prototype);
+Fashion.Phone.prototype.constructor = Fashion.Phone;
 
 //============================================================
 // Public interface
@@ -45,7 +45,7 @@ Fashion.phone.prototype.constructor = Fashion.phone;
  * @param {boolean} [destroyChildren=true] - If true `destroy` will be invoked on each removed child.
  * @param {boolean} [soft=false] - A 'soft destroy' (set to true) doesn't remove this group from its parent or null the game reference. Set to false and it does.
  */
-Fashion.phone.prototype.destroy = function (destroyChildren, soft) {
+Fashion.Phone.prototype.destroy = function (destroyChildren, soft) {
 
     Phaser.Group.prototype.destroy.call(this, destroyChildren, soft);
 };
