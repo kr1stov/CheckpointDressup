52e18373d7266d0ce2b7fbb2f576b36ed7a8d49f
Michel Wacker <mw@gentletroll.com>
2016-08-07 13:36:55 +0200

    position garment under mouse
    Signed-off-by: Michel Wacker <mw@gentletroll.com>

--- src/truck/Truck.js
+++ src/truck/Truck.js
@@ -103,7 +103,7 @@ Fashion.Truck.prototype.renderGarments = function (garments)
         garment.anchor.setTo(0.5, 0);
         // make garments dragable
         garment.inputEnabled = true;
-        garment.input.enableDrag();
+        garment.input.enableDrag(true, true, true);
 
         garment.events.onDragStart.add(this.startGarmentDrag, this);
         garment.events.onDragStop.add(this.stopGarmentDrag, this);
@@ -121,9 +121,9 @@ Fashion.Truck.prototype.renderGarments = function (garments)
  * @memberof Fashion.Truck
  * @private
  */
-Fashion.Truck.prototype.startGarmentDrag = function (garment)
+Fashion.Truck.prototype.startGarmentDrag = function (garment, pointer)
 {
-    garment.bringToTop();
+    
 };
 /**
  *
